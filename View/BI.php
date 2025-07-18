e207c3c7554f12f14bc33b57be048d3c09a812d9
Jean Sencion <156958838+Jean31XD@users.noreply.github.com>
2025-07-18 11:35:00 -0400

    Subida

--- View/BI.php
+++ View/BI.php
@@ -9,7 +9,7 @@ date_default_timezone_set('America/Santo_Domingo');
 // Verificar si el usuario está autenticado
 
 
-if (!isset($_SESSION['usuario'], $_SESSION['pantalla']) || $_SESSION['pantalla'] != 6) {
+if (!isset($_SESSION['pantalla']) || !in_array($_SESSION['pantalla'], [0, 3, 5, 6])) {
     header("Location: ../index.php");
     exit();
 }
