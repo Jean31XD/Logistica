ba1528b019a5cf83dfb8fbcb32c9a2f2d8ff18a1
Jean Sencion <156958838+Jean31XD@users.noreply.github.com>
2025-08-11 10:27:50 -0400

    Modulo de administracion de usuarios

--- View/Gestion_de_usuario.php
+++ View/Gestion_de_usuario.php
@@ -259,7 +259,7 @@ if (isset($_GET['cedula_consulta']) && !empty($_GET['cedula_consulta'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Administración Unificado ✨</title>
+    <title>Panel de gestion de usuarios </title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
