5a2c5ca534262f586f69b00ad68b9330b5d79d31
Jean Sencion <156958838+Jean31XD@users.noreply.github.com>
2025-08-11 15:13:02 -0400

    Cambios

--- View/Pantalla.php
+++ View/Pantalla.php
@@ -34,7 +34,7 @@ if (isset($_GET['ajax'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pantalla de Tíckets - Monitor</title>
+    <title>Pantalla de Tíckets</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
@@ -115,7 +115,6 @@ if (isset($_GET['ajax'])) {
     </header>
 
     <div class="main-container text-center">
-        <h1 class="titulo-principal animate__animated animate__fadeInUp">MONITOR DE TICKETS</h1>
         <div class="tabla-container animate__animated animate__fadeIn" style="animation-delay: 0.5s;">
             <div class="table-responsive">
                 <table class="table">
