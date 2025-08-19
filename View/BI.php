44e990f64e1005f844a5fdd0ce76a1865721f44c
Jean Sencion <156958838+Jean31XD@users.noreply.github.com>
2025-08-19 11:20:02 -0400

    Eliminar duplicados.

--- View/BI.php
+++ View/BI.php
@@ -9,7 +9,14 @@ if (!isset($_SESSION['usuario'])) {
 }
 
 include '../conexionBD/conexion.php';
+
 if (!$conn) die("Error de conexión: " . print_r(sqlsrv_errors(), true));
+
+$sp = "{CALL spEliminarDuplicadosFactura}";
+$stmt = sqlsrv_query($conn, $sp);
+if ($stmt === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
 
 // Obtenemos los datos para los SELECT la primera vez que carga la página
 $transportistas = [];
