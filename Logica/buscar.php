33898f50610196f4fde59ea38398d753f110d338
Jean Sencion <156958838+Jean31XD@users.noreply.github.com>
2025-09-02 15:43:38 -0400

    Cambio en buscar

--- Logica/buscar.php
+++ Logica/buscar.php
@@ -1,42 +1,23 @@
 <?php
-
-require_once __DIR__ . '/../conexionBD/conexion.php';
-
-
+require_once "../Conexion/conexion.php";
 
 $termino = trim($_GET["q"] ?? "");
 
-
-
 $resultados = [];
 
-
-
 if (!empty($termino)) {
-$sql = "SELECT TOP 20 itemid, description, itembarcode, unitid, Inventario_Listo
- FROM listo_inventario WHERE itemid LIKE ? OR itembarcode LIKE ?"; 
+    $sql = "SELECT TOP 20 itemid, description, itembarcode, unitid, Inventario_Listo
+            FROM listo_inventario
+            WHERE itemid LIKE ? OR itembarcode LIKE ?";
+    $params = ["%$termino%", "%$termino%"];
 
-$params = ["%$termino%", "%$termino%"];
-
-
-
-$stmt = sqlsrv_query($conn, $sql, $params);
-
-if ($stmt !== false) {
-
-while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-
-$resultados[] = $row;
-
- }
-
- }
-
+    $stmt = sqlsrv_query($conn, $sql, $params);
+    if ($stmt !== false) {
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $resultados[] = $row;
+        }
+    }
 }
 
-
-
 header("Content-Type: application/json; charset=UTF-8");
-
 echo json_encode($resultados);
-
