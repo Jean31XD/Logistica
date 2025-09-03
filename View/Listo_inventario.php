c0d789f6ccef9e7ac384ea575fbd58000a83c39a
Jean Sencion <156958838+Jean31XD@users.noreply.github.com>
2025-09-03 14:43:27 -0400

    CAmbio

--- View/Listo_inventario.php
+++ View/Listo_inventario.php
@@ -43,7 +43,7 @@
     <div class="container">
         <div class="search-container">
             <div class="text-center mb-4">
-             <img src="IMG\Logo Listo - Negro.png" class="img-fluid mb-3" alt="LOGO" style="max-width: 300px;">
+             <img src="IMG/LOGO MC - BLANCO.png" class="img-fluid mb-3" alt="LOGO" style="max-width: 300px;">
                 <p class="text-muted">Ingresa el MC o el codigo de barras para encontrar un producto.</p>
             </div>
 
